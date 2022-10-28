85885c7d0227b87b91f06d097264657a21a7dcec
Damla Varavir <damla.varavir@sparsetechnology.com>
2022-10-28 16:33:08 +0300

    manifest updated & sw added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "./App.css"
-import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.min.css"
+//import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+//import * as serviceWorker from "../service-worker";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,4 +14,21 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.register("service-worker.js").then(registration => {
+//     console.log("it worked", registration)
+//   }).catch(error => { console.log("fail",error) });
+// }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../service-worker.js').then(function(reg) {
+      console.log('Successfully registered service worker', reg);
+  }).catch(function(err) {
+      console.warn('Error whilst registering service worker', err);
+  });
+  }
+
+
+//serviceWorker.register();
+
+//reportWebVitals();
