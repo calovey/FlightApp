819abfc286910fa75b6aed40ca34339e4b07e77a
calovey <d_varavir@hotmail.com>
2022-11-21 20:43:56 +0300

    StoreManager added

--- src/index.js
+++ src/index.js
@@ -14,10 +14,18 @@ root.render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener("load", () => {
-  navigator.serviceWorker.register('/service-worker.js')
-  .then(reg => console.log("Service worker registered"))
-  .catch(err => console.error(`Service Worker Error: ${err}`));
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log("Service worker registered"))
+      .catch(err => console.error(`Service Worker Error: ${err}`));
   });
 } else {
   console.log("Service Worker is not supported by browser.");
 }
+
+if (navigator.storage && navigator.storage.persist)
+  navigator.storage.persist().then(function (persistent) {
+    if (persistent)
+      console.log("Storage will not be cleared except by explicit user action");
+    else
+      console.log("Storage may be cleared by the UA under storage pressure.");
+  });
