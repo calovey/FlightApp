403376049ca1f67b13d7771015294e13c54de891
calovey <d_varavir@hotmail.com>
2023-01-12 17:20:26 +0300

    Custom notification added

--- src/index.js
+++ src/index.js
@@ -15,10 +15,24 @@ root.render(
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/service-worker.js");
+    showNotification();
   });
 } else {
   console.log("Service Worker is not supported by browser.");
 }
 
-if (navigator.storage && navigator.storage.persist) navigator.storage.persist();
-Notification.requestPermission();
+function showNotification() {
+  if (navigator.storage && navigator.storage.persist) navigator.storage.persist();
+ Notification.requestPermission((result) => {
+    if (result === "granted") {
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification("Flight App", {
+          body: "Cabin crew ready for take off!",
+          icon: "../icon-192x192.png",
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: "vibration-sample",
+        });
+      });
+    }
+  });
+}
